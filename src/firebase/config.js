716dec3a9c531282f4d9685967bd9d8f6223b897
carolinechoi <fromfontana@gmail.com>
2021-03-17 22:17:06 -0700

    update to json

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,13 @@ import 'firebase/storage';
 import 'firebase/firestore';
 
 var firebaseConfig = {
-  apiKey: "AIzaSyDLmnoBvnjOQ-AaJgDw1DnVBiTETbcp8LI",
-  authDomain: "the-net-ninja-sandbox.firebaseapp.com",
-  databaseURL: "https://the-net-ninja-sandbox.firebaseio.com",
-  projectId: "the-net-ninja-sandbox",
-  storageBucket: "the-net-ninja-sandbox.appspot.com",
-  messagingSenderId: "485942827092",
-  appId: "1:485942827092:web:1811d9d8f1f5fabcd5b5c1"
+  apiKey: "AIzaSyAqMwPT_bPKLLPLKewOkTcn519zl2ubOLo",
+  authDomain: "test-a413e.firebaseapp.com",
+  projectId: "test-a413e",
+  storageBucket: "test-a413e.appspot.com",
+  messagingSenderId: "237953598904",
+  appId: "1:237953598904:web:8282e1a63440d53c843b4d",
+  measurementId: "G-ZYFT97LVZ8"
 };
 
 // Initialize Firebase
